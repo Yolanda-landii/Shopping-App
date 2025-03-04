f10988e54cc604d66840be3dfc4878bf8df3df8e
Yolanda-landii <landiimabotja2000@gmail.com>
2025-03-04 15:40:53 +0200

    modified the landing page

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import bcrypt from 'bcryptjs';
 import { useNavigate } from 'react-router-dom';
-import groceryImage from '../../assets/grocery-shopping-apps.png';
+import groceryImage from '../../assets/list.jpg';
 import './Auth.css';
 
 export default function Login() {
--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import bcrypt from 'bcryptjs';
-import groceryImage from '../../assets/grocery-shopping-apps.png';
+import groceryImage from '../../assets/list.jpg';
 
 import './Auth.css';
 
